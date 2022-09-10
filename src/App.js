16fce3d42c4e4ee6b62f2e98294f7f65be6c8103
maina lawrence <mainalawrence32@gmail.com>
2022-09-10 11:23:33 +0300

    app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HomePage from './Components/HomePage'
+import HeaderComponent from './Components/Header-Footer/HeaderComponent'
+
+function App() {
+  return (
+    <div className="container">
+      <HeaderComponent></HeaderComponent>
+     <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
